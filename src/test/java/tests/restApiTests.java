3939b017f9c3dcbdafd7fdd30ae9c106771dd4ea
alena.porohova <alena.porohova@lamoda.ru>
2023-06-06 22:44:43 +0300

    add rest_api tests

--- src/test/java/tests/restApiTests.java
+++ src/test/java/tests/restApiTests.java
@@ -1,22 +1,93 @@
 package tests;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.Matchers.is;
 
-
-
-
-
-
 public class restApiTests {
-    /*
-    1. Request https://reqres.in/
-     */
+    @BeforeEach
+    public void beforeEach(){
+        baseURI ="https://reqres.in/api";
+    }
     @Test
     void checkSingleUser () {
-      get("https://reqres.in/")
+        Integer expectedId = 2;
+        Integer actualId = given()
+                .get("/users/2")
                 .then()
-                .body("total", is(20));
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().path("data.id");
+    assertEquals(expectedId,actualId);
+    }
+    @Test
+    void checkUserNotFound () {
+                get("/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+    @Test
+    void createUser () {
+        String requestBody = "{\n" +
+                "    \"name\": \"John\",\n" +
+                "    \"job\": \"Teacher\"\n" +
+                "}";
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(requestBody)
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("John"));
+    }
+    @Test
+    void createUserNegative () {
+        String requestBody = "{\n" +
+                "    \"name\": \n" +
+                "    \"job\": \"Teacher\"\n" +
+                "}";
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(requestBody)
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400);
+    }
+    @Test
+    void updateUser () {
+        String requestBody = "{\n" +
+                "    \"name\": \"John\",\n" +
+                "    \"job\": \"Director\"\n" +
+                "}";
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(requestBody)
+                .when()
+                .patch("/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("job", is("Director"));
     }
 }
